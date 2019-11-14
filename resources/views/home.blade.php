b2107826074c483e5657129773e18610343384cd
mohsenalipoor <mohsenalipoor1373@gmail.com>
2019-11-14 04:21:21 +0330

    Create SistemBimee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,22 @@
 @extends('layouts.admin')
-
-
+@php
+$users = \App\User::count();
+$userbime = \Modules\UsersBime\Entities\UsersBime::count();
+$bime = \Modules\Bime\Entities\Bime::count();
+@endphp
 @section('content')
     <div class="row tile_count">
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
             <span class="count_top"><i class="fa fa-user"></i> تعداد کاربران</span>
-            <div class="count">2500</div>
-            <span class="count_bottom"><i class="green">4% </i> از هفته گذشته</span>
+            <div class="count">{{$users}}</div>
         </div>
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-clock-o"></i> میانگین زمان</span>
-            <div class="count">123.50</div>
-            <span class="count_bottom"><i class="green"><i
-                        class="fa fa-sort-asc"></i>3% </i> از هفته گذشته</span>
+            <span class="count_top"><i class="fa fa-user"></i>بیمه های موجود</span>
+            <div class="count green">{{$bime}}</div>
         </div>
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> مجموع مردان</span>
-            <div class="count green">2,500</div>
-            <span class="count_bottom"><i class="green"><i
-                        class="fa fa-sort-asc"></i>34% </i> از هفته گذشته</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> مجموع زنان</span>
-            <div class="count">4,567</div>
-            <span class="count_bottom"><i class="red"><i
-                        class="fa fa-sort-desc"></i>12% </i> از هفته گذشته</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> مجموعه کلی</span>
-            <div class="count">2,315</div>
-            <span class="count_bottom"><i class="green"><i
-                        class="fa fa-sort-asc"></i>34% </i> از هفته گذشته</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> مجموع اتصالات</span>
-            <div class="count">7,325</div>
-            <span class="count_bottom"><i class="green"><i
-                        class="fa fa-sort-asc"></i>34% </i> از هفته گذشته</span>
+            <span class="count_top"><i class="fa fa-user"></i>بیمه شدگان</span>
+            <div class="count green">{{$userbime}}</div>
         </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Gentelella Alela! | </title>
-
+    <title>سامانه بیمه</title>
     <!-- Bootstrap -->
     <link href="{{asset('/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('/vendors/bootstrap-rtl/dist/css/bootstrap-rtl.min.css')}}" rel="stylesheet">
